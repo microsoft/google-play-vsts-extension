e8d5cce990c1e12ce92fc62bcf607678ddc822cb
Lucas Killgore <lukillgo@microsoft.com>
2019-08-08 17:30:10 -0400

    Bumped versions for the other tasks

--- Tasks/google-play-release/GooglePlay.ts
+++ Tasks/google-play-release/GooglePlay.ts
@@ -110,27 +110,23 @@ async function run() {
             tl.debug(`Uploading ${apkFileList.length} APK(s).`);
             requireTrackUpdate = true;
 
-            let tryUploadMappingFile = tl.getBoolInput('shouldUploadMappingFile', false);
             for (const apkFile of apkFileList) {
                 tl.debug(`Uploading APK ${apkFile}`);
                 const apk: googleutil.Apk = await googleutil.addApk(edits, packageName, apkFile);
                 tl.debug(`Uploaded ${apkFile} with the version code ${apk.versionCode}`);
                 apkVersionCodes.push(apk.versionCode);
+            }
 
-                if (tryUploadMappingFile) {
-                    const mappingFilePattern = tl.getPathInput('mappingFilePath', false);
-                    tl.debug(`Mapping file pattern: ${mappingFilePattern}`);
+            if (apkVersionCodes.length > 0 && tl.getBoolInput('shouldUploadMappingFile', false)) {
+                const mappingFilePattern = tl.getPathInput('mappingFilePath', false);
+                tl.debug(`Mapping file pattern: ${mappingFilePattern}`);
 
-                    const mappingFilePath = resolveGlobPath(mappingFilePattern);
-                    tl.checkPath(mappingFilePath, 'mappingFilePath');
-                    console.log(tl.loc('FoundDeobfuscationFile', mappingFilePath));
-                    tl.debug(`Uploading mapping file ${mappingFilePath}`);
-                    await googleutil.uploadDeobfuscation(edits, mappingFilePath, packageName, apk.versionCode);
-                    tl.debug(`Uploaded ${mappingFilePath} for APK ${apkFile}`);
-
-                    // Only upload the first / main APK
-                    tryUploadMappingFile = false;
-                }
+                const mappingFilePath = resolveGlobPath(mappingFilePattern);
+                tl.checkPath(mappingFilePath, 'mappingFilePath');
+                console.log(tl.loc('FoundDeobfuscationFile', mappingFilePath));
+                tl.debug(`Uploading mapping file ${mappingFilePath}`);
+                await googleutil.uploadDeobfuscation(edits, mappingFilePath, packageName, apkVersionCodes[0]);
+                tl.debug(`Uploaded ${mappingFilePath} for APK ${apkFile}`);
             }
         } else {
             tl.debug(`Getting APK version codes of ${apkFileList.length} APK(s).`);
