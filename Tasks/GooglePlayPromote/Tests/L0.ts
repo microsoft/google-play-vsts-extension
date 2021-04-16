926147cd839d1b533dcf7003e994b05436c9b9d0
Felix Wiemann <13033994+FelixWiemann@users.noreply.github.com>
2021-04-16 12:54:41 +0200

    [GooglePlayPromote] Added possibility to specify version code to promote (#264)
    * addad possibility to specify version code to promote
    
    when promoting an app from one track to the other, previously the latest
    release on the source track was promoted.
    added a possibility to specify which version code to promote from the
    source track.
    if no version code is specified, the latest version on source track is
    used
    if a version code is specified, the apk of that version code will be
    promoted.
    if there is no apk with that version code on the source track, the task
    will fail
    
    * Updated ReadMe to reflect new versionCode Option
    
    * fixed an issue with version code not beeing the latest of track
    
    when the given version code was not the latest of track, the promote
    would fail
    
    error handling whether version code is exsitant is now done by google
    api instead of handling it in the task itself.
    
    * added log line again after accidentally deleting it
    
    * more useful tests and don't overwrite releasenotes
    
    don't overwrite release notes if a version code is given.
    better testing
    
    * forgot to add new test file

--- Tasks/GooglePlayPromote/Tests/L0.ts
+++ Tasks/GooglePlayPromote/Tests/L0.ts
@@ -55,4 +55,27 @@ describe('L0 Suite google-play-promote', function () {
         assert(testRunner.succeeded, 'task should have succeeded');
         done();
     });
+
+    it('test correct release with version code selected', (done) => {
+        const testFile = path.join(__dirname, 'L0PromoteWithVersionCode.js');
+        const testRunner = new ttm.MockTestRunner(testFile);
+
+        testRunner.run();
+
+        assert(testRunner.stdOutContained('Update track: {"releases":[{"status":"inProgress","versionCodes":[120],"releaseNotes":[{"text":"Updated Release","language":"en-US"}]}]}'), 'Did not print the expected message: ' + JSON.stringify(testRunner));
+        assert(testRunner.succeeded, 'task should have succeeded');
+        done();
+    });
+
+    it('test correct release with no version code selected', (done) => {
+        const testFile = path.join(__dirname, 'L0PromoteNoVersionCode.js');
+        const testRunner = new ttm.MockTestRunner(testFile);
+
+        testRunner.run();
+
+        assert(testRunner.stdOutContained('Update track: {"releases":[{"status":"inProgress","versionCodes":[123],"releaseNotes":[{"text":"release contents","language":"en-US"}]}]}'), 'Did not print the expected message: ' + JSON.stringify(testRunner));
+        assert(testRunner.succeeded, 'task should have succeeded');
+        done();
+    });
+
 });
---
+++ Tasks/GooglePlayPromote/Tests/L0PromoteNoVersionCode.ts
@@ -0,0 +1,51 @@
+import * as tmrm from 'azure-pipelines-task-lib/mock-run';
+import * as sinon from 'sinon';
+
+import path = require('path');
+
+const taskPath = path.join(__dirname, '..', 'google-play-promote.js');
+const tr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
+
+tr.setInput('authType', 'ServiceEndpoint');
+tr.setInput('serviceEndpoint', 'myServiceEndpoint');
+tr.setInput('packageName', 'myPackageName');
+tr.setInput('destinationTrack', 'alpha');
+tr.setInput('sourceTrack', 'production');
+tr.setInput('userFraction', '1.0');
+
+tr.registerMock('./googleutil', {
+    publisher: {
+        edits: {
+            commit: () => Promise.resolve({ data: {} })
+        }
+    },
+    getJWT: () => ({ authorize: () => sinon.stub() }),
+    updateGlobalParams: () => ({}),
+    getNewEdit: () => Promise.resolve({}),
+
+    updateTrack: (_edits, _packageName, _track, _versionCode, _userFraction, releaseNotes?) => Promise.resolve({
+        releases: [
+            {
+                status: 'inProgress',
+                versionCodes: _versionCode,
+                releaseNotes: releaseNotes
+            }
+        ]
+    }),
+
+    getTrack: () => Promise.resolve({
+        releases: [
+            {
+                status: 'inProgress',
+                versionCodes: [123],
+                releaseNotes: [
+                    { text: 'release contents', language: 'en-US' }
+                ]
+            }
+        ]
+    })
+});
+
+process.env['ENDPOINT_AUTH_myServiceEndpoint'] = '{ "parameters": {"username": "myUser", "password": "myPass"}, "scheme": "UsernamePassword"}';
+
+tr.run();
