5d3a9fabc6831bd2744903e5d6e2f2f3d4b666ef
Ivan Golubev <v-igolubev@microsoft.com>
2022-03-02 09:58:15 +0300

    Remove gulp

--- make-util.js
+++ make-util.js
@@ -1133,4 +1133,45 @@ var getTaskNodeVersion = function(buildPath, taskName) {
 }
 exports.getTaskNodeVersion = getTaskNodeVersion;
 
+var toOverrideString = function(object) {
+    return JSON.stringify(object).replace(/"/g, '\\"');
+}
+
+exports.toOverrideString = toOverrideString;
+
+var createExtension = function(manifest, publish) {
+    console.log('Installing task dependencies...');
+
+    var rootPath = process.cwd(); 
+    var tasksPath = path.join(rootPath, 'Tasks');
+    var tasks = fs.readdirSync(tasksPath);
+    console.log(tasks.length + ' tasks found.')
+    tasks.forEach(function(task) {
+        console.log('Processing task ' + task);
+        cd(path.join(tasksPath,task));
+
+        console.log('Installing PRODUCTION npm dependencies for task (' + task + ')...');
+
+        run('npm install --only=prod');
+    });
+
+    cd(rootPath);
+    
+    matchRemove('**/Tests', path.join(__dirname, '_build/Tasks/'));
+    matchRemove('**/*.js.map', path.join(__dirname, '_build/Tasks/'));
+    matchRemove('**/*.d.ts', path.join(__dirname, '_build/Tasks/'));
+
+    console.log('Creating vsix...');
+
+    var createCommand = "node ./node_modules/tfx-cli/_build/app.js extension create --manifest-globs vsts-extension-google-play.json --override " + toOverrideString(manifest);
+
+    if (publish === true) {
+        createCommand += " --share-with mobiledevops x04ty29er --token $PUBLISH_ACCESSTOKEN"
+    }
+    
+    run(createCommand);
+}
+
+exports.createExtension = createExtension;
+
 //------------------------------------------------------------------------------
